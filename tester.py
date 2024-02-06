c8f146ce583b722e6e816e635bad8134003a7818
Zoe Zelkha <zzelkha@gmail.com>
2024-02-06 20:06:54 +0100

    added tester file

---
+++ tester.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    print('Hello world!')
+    return 'hello'
+
+if __name__ == '__main__':
+    app.run(debug=True)
